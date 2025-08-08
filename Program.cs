52fd93120f66dc43f5705decbe9414dbe9b4cfe7
tatojastine-n <jajanicoletato@gmail.com>
2025-08-08 22:00:40 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalUntilStopWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers to sum (type 'stop' to finish):");
+
+            int sum = 0;
+            string input;
+
+            do
+            {
+                Console.Write("Enter a number: ");
+                input = Console.ReadLine();
+
+                if (input.Equals("stop", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                if (int.TryParse(input, out int number))
+                {
+                    sum += number;
+                }
+                else
+                {
+                    Console.WriteLine($"Ignoring invalid input: '{input}'");
+                }
+            } while (true);
+
+            Console.WriteLine($"\nTotal sum: {sum}");
+        }
+    }
+}
